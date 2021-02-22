e1e90c9eb52cbd6179a1ecbb5039a51910c46a4b
Daniel Ivanescu <ivanescm@uni.coventry.ac.uk>
2021-02-22 21:54:03 +0000

    Util: Add more debugging features.

--- util/util.h
+++ util/util.h
@@ -14,11 +14,22 @@ void calculateTimeDelta();
 
 void smoothLerp(double &from, double &to);
 
+#define WIFI_TASK_PRIORITY 4
+#define HTTP_TASK_PRIORITY 3
+#define MP3_TASK_PRIORITY 2
+#define I2S_TASK_PRIORITY 1
+
 #if defined(CONFIG_DEBUG_VGA_PROJ)
-#define LOG_FN(e, fn) { ESP_LOGI(TAG, "%s(%d): %s returned 0x%0x", __func__, __LINE__, fn, e); }
-#define LOG_PTR(ptr, fn) { ESP_LOGI(TAG, "%s(%d): %s returned %p", __func__, __LINE__, fn, ptr); }
+#define LOG_INFO(msg, ...) { ESP_LOGI(TAG, "%s(%d): " msg, __func__, __LINE__, __VA_ARGS__); }
+#define LOG_ERR(msg, ...) { ESP_LOGE(TAG, "%s(%d): " msg, __func__, __LINE__, __VA_ARGS__); }
+#define LOG_FN(e, fn) { if (e != ESP_OK) ESP_LOGE(TAG, "%s(%d): %s returned 0x%0x", __func__, __LINE__, fn, e); }
+#define LOG_PTR(ptr, fn) { if (!ptr) ESP_LOGE(TAG, "%s(%d): %s returned null", __func__, __LINE__, fn); }
 #else
+#define LOG_INFO(msg, ...)
+#define LOG_ERR(msg, ...)
 #define LOG_FN(e, fn)
 #define LOG_PTR(ptr, fn)
 #endif
 
+#define LOG_FN_GOTO_IF_ERR(e, fn, label) { LOG_FN(e, fn); if (e != ESP_OK) goto label; }
+#define LOG_FN_GOTO_IF_NULL(e, fn, label) { if (!e) { LOG_PTR(e, fn); goto label; } }
