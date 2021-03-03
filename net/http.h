61d53647a3838eaf7972ef90758900ba98594a52
Daniel Ivanescu <ivanescm@uni.coventry.ac.uk>
2021-03-03 19:13:44 +0000

    Code coverage: Remove HTTP event handler, submenu definitions and main.cpp from the codebase as these will now be handled on a per-app basis.

--- net/http.h
+++ net/http.h
@@ -14,7 +14,13 @@ extern QueueHandle_t radioQueueTx, otaQueueTx;
 
 extern esp_http_client_handle_t httpClient;
 
-esp_err_t initHTTP();
+/**
+ * @brief Initialize HTTP client for user applications. The client can then accept get requests by sending
+ *        data to httpQueueRx. NOTE: Users must implement their own event handler to make proper use of the client.
+ *
+ * @param httpEventHandler Function pointer to the event handler of the HTTP client.
+ */
+esp_err_t initHTTP(esp_err_t (*httpEventHandler)(esp_http_client_event_t* event));
 
 enum http_queue_tx_flag {
 	HTTP_QUEUE_TX_REQUEST_RADIO_LIST,
@@ -26,5 +32,3 @@ enum radio_queue_rx_flag {
 	RADIO_QUEUE_RX_RADIO_STATION,
 	RADIO_QUEUE_RX_FINISHED_OP
 };
-
-void httpGet(char *url);
--- ux/submenus/submenus.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <submenus/wifi_menu.h>
-#include <submenus/app_menu.h>
-#include <submenus/radio_menu.h>
