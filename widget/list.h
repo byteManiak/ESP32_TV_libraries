df9940efae7720c1ace6a3917a181636060b69c5
ivanescm <ivanescm@uni.coventry.ac.uk>
2021-03-10 23:14:26 +0000

    Widgets: Make text in hovered list element scrollable in case it doesn't fit on screen.

--- widget/list.h
+++ widget/list.h
@@ -12,8 +12,8 @@ template <typename T>
 class List : public Widget
 {
 public:
-    List(VGAExtended *vga, int16_t x, int16_t y, int8_t maxListSize, bool centeredText = false)
-        : Widget(vga, x, y), maxListSize{maxListSize}, centeredText{centeredText} {}
+    List(VGAExtended *vga, int16_t x, int16_t y, int8_t maxListSize, bool centeredText = false, bool scrollText = false)
+        : Widget(vga, x, y), maxListSize{maxListSize}, centeredText{centeredText}, scrollText{scrollText} {}
 
     void addElement(T elem)
     {
@@ -57,6 +57,8 @@ public:
             heap_caps_free(currentElem);
         }
         listElements.clear();
+
+        hoverTime = getMillis();
     }
 
     T &getElement()
@@ -74,11 +76,13 @@ public:
             {
                 currentElement--;
                 if (currentElement < 0) currentElement = 0;
+                hoverTime = getMillis();
             }
             if (isKeyPressed(Down_key))
             {
                 currentElement++;
                 if (currentElement > maxListSize-1) currentElement = maxListSize-1;
+                hoverTime = getMillis();
             }
 
             if (isKeyPressed(Enter_key)) status = currentElement;
@@ -94,14 +98,26 @@ public:
             smoothLerp(currentX, to);
             for(int i = 0; i < listElements.size(); i++)
             {
-                if (i == currentElement) vga->setTextColor(vga->RGB(0x00AAFF), vga->backColor);
-                else vga->setTextColor(vga->RGB(0xFFFFFF), vga->backColor);
+                if (i == currentElement)
+                {
+                    vga->setTextColor(vga->RGB(0x00AAFF), vga->backColor);
+
+                    // Start scrolling text after 2 seconds of hovering over it
+                    int64_t scrollTime = getMillis() - hoverTime;
+                    if (scrollText && scrollTime > 2000)
+                        offsetIndex = ((scrollTime - 2000) / 500) % (strlen(listElements[i])-40);
+                }
+                else
+                {
+                    vga->setTextColor(vga->RGB(0xFFFFFF), vga->backColor);
+                    offsetIndex = 0;
+                }
 
                 int xcoord = currentX;
                 if (centeredText) xcoord -= vga->font->charWidth * strlen(listElements[i]) / 2.f;
 
                 vga->setCursor(xcoord, baseY + i * (vga->font->charHeight+1));
-                vga->drawText(listElements[i]);
+                vga->drawText(listElements[i]+offsetIndex);
 
                 int ylinecoord = baseY + (i+1) * (vga->font->charHeight+1) - 1;
                 vga->drawLine(xcoord, ylinecoord, xcoord+vga->xres, ylinecoord, vga->RGB(0x00FFAA));
@@ -116,4 +132,9 @@ private:
     heap_caps_vector<T> listElements;
 
     bool centeredText;
+    bool scrollText;
+
+    // Optionally used to scroll through text that does not fit on text
+    int64_t hoverTime;
+    uint8_t offsetIndex = 0;
 };
