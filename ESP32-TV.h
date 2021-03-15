309398fbf52bec737bc448f4ebea6cdc8681d111
ivanescm <ivanescm@uni.coventry.ac.uk>
2021-03-15 23:30:10 +0000

    ESP Init: Add main include file which ensures the right parameters are being set at compile time.

---
+++ ESP32-TV.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <ESP32Lib.h>
+#include <Ressources/CodePage437_8x8.h>
+
+#include <io/ps2.h>
+#include <io/sound.h>
+
+#include <memory/alloc.h>
+
+#include <net/wifi.h>
+#include <net/http.h>
+
+#include <vga/vga.h>
+
+#include <util/log.h>
+#include <util/appinfo.h>
+#include <util/numeric.h>
+#include <util/queues.h>
+
+#include <stats/stats.h>
