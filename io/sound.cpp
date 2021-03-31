04763b6cd9fd0ce7f728199a6e42c78056ed2ec1
ivanescm <ivanescm@uni.coventry.ac.uk>
2021-03-31 18:26:32 +0100

    Sound: Add support for playing basic music through the onboard buzzers.

--- io/sound.cpp
+++ io/sound.cpp
@@ -6,6 +6,8 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include <driver/i2s.h>
+#include <driver/ledc.h>
+#include <esp_timer.h>
 #include <board.h>
 #include <audio_hal.h>
 #include <audio_element.h>
@@ -16,6 +18,7 @@
 
 #include <memory/alloc.h>
 #include <util/log.h>
+#include <util/numeric.h>
 #include <util/queues.h>
 
 static const char *TAG = "sound";
@@ -144,4 +147,124 @@ void initSound()
 	boardHandle = audio_board_init();
 	audio_hal_ctrl_codec(boardHandle->audio_hal, AUDIO_HAL_CODEC_MODE_DECODE, AUDIO_HAL_CTRL_START);
 	audioQueue = xQueueCreate(4, sizeof(queue_message*));
+}
+
+void initBuzzers()
+{
+	ledc_timer_config_t pwm1Config = {};
+	pwm1Config.speed_mode = LEDC_HIGH_SPEED_MODE;
+	pwm1Config.duty_resolution = LEDC_TIMER_12_BIT;
+	pwm1Config.clk_cfg = LEDC_USE_APB_CLK;
+	pwm1Config.freq_hz = 5000;
+	pwm1Config.timer_num = LEDC_TIMER_0;
+
+	ledc_timer_config_t pwm2Config = {};
+	pwm2Config.speed_mode = LEDC_HIGH_SPEED_MODE;
+	pwm2Config.duty_resolution = LEDC_TIMER_12_BIT;
+	pwm2Config.clk_cfg = LEDC_USE_APB_CLK;
+	pwm2Config.freq_hz = 5000;
+	pwm2Config.timer_num = LEDC_TIMER_1;
+
+	ledc_timer_config(&pwm1Config);
+	ledc_timer_config(&pwm2Config);
+
+	ledc_channel_config_t channel1Config = {};
+	channel1Config.channel = LEDC_CHANNEL_0;
+	channel1Config.gpio_num = 18; //25;
+	channel1Config.speed_mode = LEDC_HIGH_SPEED_MODE;
+	channel1Config.timer_sel = LEDC_TIMER_0;
+	channel1Config.duty = 0;
+
+	ledc_channel_config_t channel2Config = {};
+	channel2Config.channel = LEDC_CHANNEL_1;
+	channel2Config.gpio_num = 19; //26;
+	channel2Config.speed_mode = LEDC_HIGH_SPEED_MODE;
+	channel2Config.timer_sel = LEDC_TIMER_1;
+	channel2Config.duty = 0;
+
+	// In case a PWM driver was already installed
+	ledc_fade_func_uninstall();
+
+	ledc_channel_config(&channel1Config);
+	ledc_channel_config(&channel2Config);
+}
+
+static unsigned short frequencyTable[] =
+{
+	31, 33, 35, 37, 39, 41, 44, 46, 49, 52, 55, 58, 62, 65, 69, 73, 78, 82, 87, 93, 98, 104, 110, 117, 123,
+	131, 139, 147, 156, 165, 175, 185, 196, 208, 220, 233, 247, 262, 277, 294, 311, 330, 349, 370, 392, 415,
+	440, 466, 494, 523, 554, 587, 622, 659, 698, 740, 784, 831, 880, 932, 988, 1047, 1109, 1175, 1245, 1319,
+	1397, 1480, 1568, 1661, 1760, 1865, 1976, 2093, 2217, 2349, 2489, 2637, 2794, 2960, 3136, 3322, 3520,
+	3729, 3951, 4186, 4435, 4699, 4978
+};
+
+static BuzzerMusic *musicHandle;
+
+static void soundLoop(void*);
+
+static esp_timer_handle_t soundLoopHandle = NULL;
+
+void playBuzzerMusic(BuzzerMusic *music)
+{
+	// No point playing an empty piece of music
+	if (!music || music->data_length == 0) return;
+
+	musicHandle = music;
+
+	// Replace the timer interrupt if it existed already
+	if (soundLoopHandle) esp_timer_delete(soundLoopHandle);
+
+	esp_timer_create_args_t isrTimerConfig = {};
+	isrTimerConfig.callback = soundLoop;
+	isrTimerConfig.name = "BuzzerMusic";
+
+	// Create a timer with the tick rate specified in the music piece
+	esp_timer_create(&isrTimerConfig, &soundLoopHandle);
+	esp_timer_start_periodic(soundLoopHandle, musicHandle->tick_period);
+	LOG_INFO("m");
+}
+
+static uint8_t currentTick = 0;
+static uint32_t currentIndex = 0;
+static BuzzerInstrument *leftChannelPtr, *rightChannelPtr;
+
+static void soundLoop(void*)
+{
+	// If the tick counter has just been reset, advance the note pointer and play the corresponding next note
+	if (currentTick == 0)
+	{
+		leftChannelPtr = &musicHandle->left_ch_data[currentIndex];
+		rightChannelPtr = &musicHandle->right_ch_data[currentIndex];
+
+		if (leftChannelPtr->note > 0)
+		{
+			// Prepare the frequency and duty cycle to apply to the left channel buzzer
+			ledc_set_freq(LEDC_HIGH_SPEED_MODE, LEDC_TIMER_0, frequencyTable[leftChannelPtr->note]);
+
+			if (leftChannelPtr->duty > 0) ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, leftChannelPtr->duty);
+			// If no duty has been specified, apply 50% duty
+			else ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, 2048);
+		}
+		// If the note is NONE, don't play any sound
+		else ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, 0);
+
+		if (rightChannelPtr->note > 0)
+		{
+			// Prepare the frequency and duty cycle to apply to the right channel buzzer
+			ledc_set_freq(LEDC_HIGH_SPEED_MODE, LEDC_TIMER_1, frequencyTable[rightChannelPtr->note]);
+
+			if (rightChannelPtr->duty > 0) ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_1, rightChannelPtr->duty);
+			// If no duty has been specified, apply 50% duty
+			else ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_1, 2048);
+		}
+		// If the note is NONE, don't play any sound
+		else ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_1, 0);
+
+		// Call ledc_update_duty to actually write the new PWM duty to the buzzers.
+		ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0);
+		ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_1);
+	}
+
+	currentTick = getNextInt(currentTick, musicHandle->ticks_per_note);
+	if (currentTick == 0) currentIndex = getNextInt(currentIndex, musicHandle->data_length);
 }
--- io/sound.h
+++ io/sound.h
@@ -5,3 +5,40 @@
 void initSound();
 void audioDispatchTask(void *arg);
 
+enum Note
+{
+	NONE, C1, Cs1, D1, Ds1, E1, F1, Fs1, G1, Gs1, A1, As1,
+	B1, C2, Cs2, D2, Ds2, E2, F2, Fs2, G2, Gs2, A2, As2,
+	B2, C3, Cs3, D3, Ds3, E3, F3, Fs3, G3, Gs3, A3, As3,
+	B3, C4, Cs4, D4, Ds4, E4, F4, Fs4, G4, Gs4, A4, As4,
+	B4, C5, Cs5, D5, Ds5, E5, F5, Fs5, G5, Gs5, A5, As5,
+	B5, C6, Cs6, D6, Ds6, E6, F6, Fs6, G6, Gs6, A6, As6,
+	B6, C7, Cs7, D7, Ds7, E7, F7, Fs7, G7, Gs7, A7, As7,
+	B7, C8, Cs8, D8, Ds8
+};
+
+struct BuzzerInstrument
+{
+	Note note;
+	uint16_t duty;
+};
+
+struct BuzzerMusic
+{
+	uint64_t tick_period;
+	uint8_t ticks_per_note;
+	uint32_t data_length;
+	BuzzerInstrument *left_ch_data;
+	BuzzerInstrument *right_ch_data;
+};
+
+/**
+ * @brief Initialises the onboard 5V buzzers.
+ */
+void initBuzzers();
+
+/**
+ * @brief Starts playing a music loop from the specified BuzzerMusic structure.
+ * @param music Pointer to the music data to play.
+ */
+void playBuzzerMusic(BuzzerMusic *music);
